ac73d994a161f24e4900254964590833eb38e700
Aristotle <aristotleprovatas@gmail.com>
2025-04-21 01:30:31 -0400

    initial commit

---
+++ Main/src/test/java/com/mycompany/app/MainTest.java
@@ -0,0 +1,14 @@
+
+/**
+ * Unit test for simple App.
+ */
+public class MainTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
