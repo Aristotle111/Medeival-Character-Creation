56f848c3132919071ab6a12bf2c63383a412adce
Aristotle <aristotleprovatas@gmail.com>
2025-04-27 22:42:29 -0400

    data feilds done

---
+++ Main/src/main/java/com/mycompany/app/Character.java
@@ -0,0 +1,12 @@
+package com.mycompany.app;
+public class Character {
+    public int age;
+    public String name;
+    public Gender gender;
+
+    public Character(int age, String name, Gender gender) {
+        this.age = age;
+        this.name = name;
+        this.gender = gender;
+    }
+}
