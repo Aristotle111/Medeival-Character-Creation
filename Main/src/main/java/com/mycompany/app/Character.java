635d1f84374f6af5d61996b5008c70412590cb0d
Aristotle <aristotleprovatas@gmail.com>
2025-04-27 23:33:57 -0400

    item class and getters and setters and stuff

--- Main/src/main/java/com/mycompany/app/Character.java
+++ Main/src/main/java/com/mycompany/app/Character.java
@@ -9,4 +9,64 @@ public class Character {
         this.name = name;
         this.gender = gender;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + age;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Character other = (Character) obj;
+        if (age != other.age)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (gender != other.gender)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Character [age=" + age + ", name=" + name + ", gender=" + gender + "]";
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
 }
